d21fedfbf401eefb2e7a2e173cb8b7e2a62d08be
Álvaro Cosín Cebollada <alvarocosin@Alvaros-MacBook-Air.local>
2025-10-02 12:45:13 +0200

    Styling, dark mode

--- src/components/MediaCard.jsx
+++ src/components/MediaCard.jsx
@@ -5,9 +5,15 @@ export default function MediaCard({ item }) {
     <div className="media-card">
       <img src={item.posterUrl} alt={item.title} />
       <div className="media-info">
+        {/* Top: title */}
         <h3>{item.title}</h3>
-        <p>{item.year}</p>
-        <p>Watched: {item.dateWatched}</p>
+
+        {/* Bottom: director, year, date */}
+        <div className="bottom-info">
+          <p className="director">{item.director}</p>
+          <p className="meta">{item.year}</p>
+          <p className="watched">Watched: {item.dateWatched}</p>
+        </div>
       </div>
     </div>
   );
