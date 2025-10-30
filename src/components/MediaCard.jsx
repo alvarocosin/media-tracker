eb50420552f1ac1738e70950c87a33a0aad71f79
Álvaro Cosín Cebollada <alvarocosin@Alvaros-MacBook-Air.local>
2025-10-30 10:39:15 +0100

    Fav icon

--- src/components/MediaCard.jsx
+++ src/components/MediaCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaHeart } from "react-icons/fa";
 
 export default function MediaCard({ item }) {
   return (
@@ -12,7 +13,7 @@ export default function MediaCard({ item }) {
         <div className="bottom-info">
           <p className="director">{item.director}</p>
           <p className="meta">{item.year}</p>
-          <p className="watched">Watched: {item.dateWatched}</p>
+          <p className="watched">Watched: {item.dateWatched} {item.fav && <FaHeart className="favorite" />}</p>
         </div>
       </div>
     </div>
